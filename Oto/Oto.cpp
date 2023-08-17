0d0e8cd9bd0469467152e62df57a0ae221f882cf
PhamThiLanAnh <anhphamnb1002@gmail.com>
2023-08-17 17:55:54 +0700

    update

--- Oto/Oto.cpp
+++ Oto/Oto.cpp
@@ -10,6 +10,20 @@ float headY = 0.0;
 float headZ = 0.0;
 float headRadius = .6; // Bán kính của đầu
 
+float rotationAngle = 0.0;
+
+float objectX = 0.0;//cho quay quanh trục y
+float objectY = 0.0;
+float objectZ = 0.0;
+
+float initialObjectX = 0.0; // tọa độ đầu
+float initialObjectY = 0.0;
+float initialObjectZ = 0.0;
+
+bool isMovingBack = false;// Biến để kiểm tra xem vật đang di chuyển về vị trí đầu tiên hay không
+
+//float wingY = 0.0;
+
 float maxOuterRadius = 1;
 
 float totalLengthBee = headRadius / 1.18;
@@ -22,9 +36,9 @@ bool isDrawing = false;
 bool isMaxAngleWing = false;
 
 
-float eyeX = 6;
-float eyeY = 0;
-float eyeZ = 0;
+float eyeX = 10;
+float eyeY = 7;
+float eyeZ = 15;
 float centerX = 0;
 float centerY = 0;
 float centerZ = 0;
@@ -118,8 +132,8 @@ void display() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glColor3f(0.0, 0.0, 0.0);
 		glLoadIdentity();
-
 		gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+		
 		drawOxyz();
 
 		//glutPostRedisplay();
@@ -132,7 +146,13 @@ void display() {
 		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);// nguồn sáng, hàm số, gt
 		glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
 
+
+		////////quay 360 
+		glTranslatef(objectX, objectY, objectZ);
+		glRotatef(rotationAngle, 0.0, 1.0, 0.0);
+
 		//Vẽ đầu
+
 		yellow();
 		glPushMatrix();
 		glTranslatef(headX, headY, headZ);
@@ -142,17 +162,15 @@ void display() {
 		// Vẽ thân		
 		glPushMatrix();
 		glTranslatef(headX , headY, headZ - 1.3);
-		//glRotatef(90, 0, 1, 0);
 		glScalef(.75, .75, 1);
 		glutSolidSphere(1, 20, 20);
 		glPopMatrix();
 		
 		// Sọc
 		
-		
 		glPushMatrix();
-		brown();
-		glTranslatef(headX, headY, headZ - 0.6);		
+		black();
+		glTranslatef(headX, headY, headZ - 0.65);		
 		glScalef(0.37, 0.37, 0.3);
 		glutSolidTorus(0.5, 1, 20, 20);
 		glPopMatrix();
@@ -160,7 +178,7 @@ void display() {
 		// Sọc
 
 		glPushMatrix();
-		brown();
+		black();
 		glTranslatef(headX, headY, headZ - 0.95);
 		glScalef(0.49, 0.49, 0.3);
 		glutSolidTorus(0.5, 1, 20, 20);
@@ -169,7 +187,7 @@ void display() {
 		// Sọc
 
 		glPushMatrix();
-		brown();
+		black();
 		glTranslatef(headX, headY, headZ - 1.3);
 		glScalef(0.53, 0.53, 0.3);
 		glutSolidTorus(0.5, 1, 20, 20);
@@ -178,7 +196,7 @@ void display() {
 		// Sọc
 
 		glPushMatrix();
-		brown();
+		black();
 		glTranslatef(headX, headY, headZ - 1.7);
 		glScalef(0.49, 0.49, 0.3);
 		glutSolidTorus(0.5, 1, 20, 20);
@@ -187,7 +205,7 @@ void display() {
 		// Sọc
 
 		glPushMatrix();
-		brown();
+		black();
 		glTranslatef(headX, headY, headZ - 2.0);
 		glScalef(0.35, 0.35, 0.3);
 		glutSolidTorus(0.5, 1, 20, 20);
@@ -247,7 +265,7 @@ void display() {
 		glPushMatrix();
 		glRotatef(angleWing, 0, 0, -1);
 		glRotatef(40, 0, 0, 1);
-		glTranslatef(headX, headY + headRadius * 2,  headZ - 1.2);
+		glTranslatef(headX, headY + headRadius * 2, headZ - 1.2);
 		glScalef(.1, 1.3, .5);
 		glutSolidSphere(1, 30, 30);
 		glPopMatrix();
@@ -282,12 +300,14 @@ void reshape(int w, int h) {
 	glLoadIdentity();
 	glFrustum(-1, 1, -1, 1, 1.5, 20);
 	glMatrixMode(GL_MODELVIEW);
+	//gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+	//gluLookAt(-7.0, 3.0, 5.0, 0, 0, 0, 0, 1, 0);
 }
 
 void specialKeyPress(int key, int x, int y) {
 	switch (key)
 	{
-	case GLUT_KEY_UP:
+	/*case GLUT_KEY_UP:
 		eyeY += 0.5;
 		break;
 	case GLUT_KEY_DOWN:
@@ -300,7 +320,8 @@ void specialKeyPress(int key, int x, int y) {
 		eyeZ += 0.5;
 		break;
 	default:
-			break;
+			break;*/
+
 	}
 	resetValueVariable();
 	glutPostRedisplay();
@@ -309,16 +330,28 @@ void specialKeyPress(int key, int x, int y) {
 void keyboardPress(unsigned char key, int x, int y) {
 	switch (key)
 	{
-		case 'l':
+		case 'r':/// phím r quay 360 quanh y
+            rotationAngle += 1.0; // Tăng góc quay lên 5 độ
+            if (rotationAngle >= 360.0) {
+                rotationAngle -= 360.0; // Đảm bảo góc không vượt quá 360 độ
+            }
+            glutPostRedisplay();
+            break;
+		case 'm':
+			isMovingBack = true;
+			glutPostRedisplay();
+			break;
+
+		/*case 'l':
 			eyeX += .5;
 			break;
 		case 'k':
 			eyeX -= .5;
-			break;
+			break;*/
 		case 'w':
-			headY += 0.1;
+			headY += 0.0;
 			break;
-		case 's':
+		/*case 's':
 			headY -= 0.1;
 			break;
 		case 'd':
@@ -328,7 +361,8 @@ void keyboardPress(unsigned char key, int x, int y) {
 			headZ += 0.1;
 			break;
 		default:
-			break;
+			break;*/
+	
 	}
 	if (key == 27) { // Phím ESC
 		exit(0);
@@ -352,25 +386,33 @@ void mouseClick(int button, int state, int x, int y) {
 }
 
 void update(int value) {
-	/*headY += .2;
-	headZ += .3;*/
+	//headY += .2;
+	headZ += .1;
 
 	if (!isMaxAngleWing) {
-		angleWing += .2;
+		angleWing += 2.0;
 		if (angleWing >= 90) {
 			isMaxAngleWing = true;
 		}
 	}
 	else {
-		angleWing -= .2;
+		angleWing -= 2.0;
 		if (angleWing <= 40) {
 			isMaxAngleWing = false;
 		}
+
+	}
+
+	if (isMovingBack && objectZ < headZ) {
+		objectZ -= 0.01; // Giảm tọa độ X theo tốc độ di chuyển
+	}
+	else {
+		isMovingBack = false;
 	}
 
 	resetValueVariable();
 	glutPostRedisplay();
-	glutTimerFunc(10, update, 0);
+	glutTimerFunc(50, update, 0);
 }
 
 
